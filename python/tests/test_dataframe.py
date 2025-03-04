5652818d947db679b0b4cb336b8ee20abaf7b635
Siew Kam Onn <kosiew@gmail.com>
2025-03-04 14:28:59 +0800

    Update performance test assertion for DataFrame representation to use a more precise duration threshold

--- python/tests/test_dataframe.py
+++ python/tests/test_dataframe.py
@@ -1275,9 +1275,7 @@ def test_dataframe_repr_performance(ctx):
     _ = repr(df)
     duration = time.time() - start_time
 
-    assert (
-        duration * 100 < 1.0
-    ), f"repr() took {duration:.2f} seconds, which is too long"
+    assert duration < 0.003, f"repr() took {duration:.5f} seconds, which is too long"
 
 
 def test_format_column_name(df):
